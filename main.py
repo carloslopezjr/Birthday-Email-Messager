34d6eac2fa6eba54c22ed43c14f752525f2078a6
bravodude <85579815+bravodude@users.noreply.github.com>
2023-06-19 19:55:25 -0500

    init project

---
+++ main.py
@@ -0,0 +1,4 @@
+from datetime import datetime # import the time library into the program
+
+currentTime = datetime.now().time().strftime("%H:%M:%S") # displays current time with specific formatting
+
