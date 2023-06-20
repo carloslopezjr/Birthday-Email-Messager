f612a82a2c2f85887753f8a8105ced613e077a3d
carloslopezjr <85579815+carloslopezjr@users.noreply.github.com>
2023-06-19 22:48:02 -0500

    add new function, and adjust times

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from datetime import datetime  # import the time library into the program
 import time
 
 # 11am == 11:00
+
+
 def currentTime():
 
     global currentDate
@@ -13,7 +15,7 @@ def currentTime():
 
     # get the current date in the / / / format
     currentDate = datetime.now().strftime("%m/%d")
-    
+
     return currentHour, currentDate
 
 
@@ -30,7 +32,7 @@ def birthdayFinder():
             # print out a specific statement including the name
 
             print(f"It's {name}'s birthday!")
-            
+
             personInfo[x]["birthdayToday"] = True
             x += 1
 
@@ -43,19 +45,37 @@ def dailyChecker():
     timer = 0
     while timer == 0:
 
-        currentTime()
+        currentTime() # run function
 
-        if currentHour == "21:34":
-            # print("It's time to check if anyone has a birthday today!")
-            birthdayFinder()
+        # if current time is 11:00
+        if currentHour == "11:00":
+
+            # check to see if it's someones birthday today
+            birthdayFinder() # run function
             timer += 1
 
         else:
+            # no birthday detected
             print(f"No Birthday Detected {currentHour}")
 
-        time.sleep(60)
+            # pauses between each while interval
+            time.sleep(60)
+        
+
+def personalizedMessage():
+
+    x = 0
+    while x < len(personInfo):
+        if personInfo[x]["birthdayToday"] == True:
+            
+            # get the value of key "name"
+            name = personInfo[x]["name"]
+            
+            print(f"Happy Birthday {name}! I hope you have a good day!")
+            x += 1
 
 
+# data base of information
 personInfo = [{
     "name": "Carlos",
     "birthday": "06/19",
@@ -68,21 +88,11 @@ personInfo = [{
         "birthday": "04/11",
         "phone": "961-775-2092",
         "birthdayToday": False
-},
-    {
-        "name": "Kyra",
-        "birthday": "06/20",
-        "phone": "961-775-2092",
-        "birthdayToday": False
 }
 ]
 
 
-
 # Outputs
-# dailyChecker()
 
-currentTime()
-birthdayFinder()
-
-print(personInfo)
+dailyChecker()
+personalizedMessage()
