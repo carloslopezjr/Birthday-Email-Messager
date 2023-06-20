cc50ec2512e2a36b5d37bebfa5e532b6c99b33f2
bravodude <85579815+bravodude@users.noreply.github.com>
2023-06-19 21:38:24 -0500

    basic functions

--- main.py
+++ main.py
@@ -1,4 +1,79 @@
-from datetime import datetime # import the time library into the program
+from datetime import datetime  # import the time library into the program
 
-currentTime = datetime.now().time().strftime("%H:%M:%S") # displays current time with specific formatting
+import time
 
+# 11am == 11:00
+def currentTime():
+
+    global currentDate
+    global currentHour
+
+    # displays current time with specific formatting
+    currentHour = datetime.now().time().strftime("%H:%M")
+
+    # get the current date in the / / / format
+    currentDate = datetime.now().strftime("%m/%d")
+    # create a list of dictionaries with people's names
+
+    # print(f"{currentDate} {currentHour}")
+
+    return currentHour, currentDate
+
+
+def birthdayFinder():
+
+    # we want to loop through the person info list
+    x = 0
+    while x < len(personInfo):
+
+        if currentDate == personInfo[x]["birthday"]:
+
+            # get the value of key "name"
+            name = personInfo[x]["name"]
+            # print out a specific statement including the name
+
+            print(f"It's {name}'s birthday!")
+            x += 1
+
+        else:
+            x += 1
+
+
+def dailyChecker():
+
+    timer = 0
+    while timer == 0:
+
+        currentTime()
+
+        if currentHour == "21:34":
+            # print("It's time to check if anyone has a birthday today!")
+            birthdayFinder()
+            timer += 1
+
+        else:
+            print(f"No Birthday Detected {currentHour}")
+
+        time.sleep(60)
+
+
+personInfo = [{
+    "name": "Carlos",
+    "birthday": "06/19",
+    "phone": "210-419-0444"
+},
+
+    {
+        "name": "Kyra",
+        "birthday": "04/11",
+        "phone": "961-775-2092"
+},
+    {
+        "name": "Kyra",
+        "birthday": "06/19",
+        "phone": "961-775-2092"
+}
+]
+
+# Outputs
+dailyChecker()
